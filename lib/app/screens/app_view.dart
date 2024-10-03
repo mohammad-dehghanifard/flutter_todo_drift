611ce418bec4ded24460b78d0f5d1a3aaa97a93a
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-03 09:39:56 -0700

    impl AppView class

---
+++ lib/app/screens/app_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_drift/app/screens/app_view.dart';
 import 'package:flutter_todo_drift/database/database.dart';
 import 'package:flutter_todo_drift/database/database_helper.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await DataBaseHelper.init();
-  runApp(const MyApp());
+  runApp(const AppView());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-    );
-  }
-}
 
